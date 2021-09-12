e1bb17306db6e74ab4a17e4599c258ed8181438f
Ayazhan <ayazhan.shansharova@mail.ru>
2021-09-12 22:07:25 +0600

    thrid commit

--- src/index.js
+++ src/index.js
@@ -4,11 +4,14 @@ import ReactDOM from 'react-dom';
 import './scss/app.scss';
 
 import App from './App';
+import { BrowserRouter, Route} from 'react-router-dom';
 
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+    <Route exact path="/" component={App}/>
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
